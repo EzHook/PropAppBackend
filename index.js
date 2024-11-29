bc90c1701b02193de257f90c1b837816d7171161
Ezhook <srctickter@gmail.com>
2024-11-29 11:52:39 +0530

    new changes

--- index.js
+++ index.js
@@ -95,10 +95,8 @@ app.get("/properties/:id", (req, res) => {
   res.json(property);
 });
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
 module.exports = app;
